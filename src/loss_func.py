3616dc2af83e21c9186800ad365bf3e530323340
Sean Hastings <sean_hastings@brown.edu>
2019-07-31 13:59:35 -0400

    cleanup

--- src/loss_func.py
+++ src/loss_func.py
@@ -18,15 +18,17 @@ class BehaviorCloneLoss(nn.Module):
         self.eps = 1e-7
 
     def forward(self, out, aux_out, target, aux_target):
+        out    = out[:,:-1]
+        target = target[:,:-1]
+    
         l2_loss = self.l2(out, target)
         l1_loss = self.l1(out, target)
 
         # For the arccos loss
         bs, n = out.shape
-        num = torch.bmm(target.view(bs,1,n), out.view(bs,n,1))
-        den = torch.bmm(torch.norm(target.view(bs,n,1),p=2,dim=1,keepdim=True),
-                        torch.norm(out.view(bs,n,1),p=2,dim=1,keepdim=True))
-        a_cos = torch.squeeze(torch.acos(torch.clamp(torch.div(num, den), 0, 1-self.eps)))
+        num = torch.bmm(target.view(bs,1,n), out.view(bs,n,1)).squeeze()
+        den = torch.norm(target,p=2,dim=1) * torch.norm(out,p=2,dim=1)
+        a_cos = torch.acos(num / den)
         c_loss = torch.mean(a_cos)
         # For the aux loss
         aux_loss = self.aux(aux_out, aux_target)
